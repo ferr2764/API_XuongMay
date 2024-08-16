8d5e9f2f432175b4a65994d972b2bef8f6de2b8b
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2024-08-16 12:17:02 +0700

    fix some error

--- XuongMay.Repositories/UOW/UnitOfWork.cs
+++ XuongMay.Repositories/UOW/UnitOfWork.cs
@@ -1,58 +1,55 @@
-﻿using System;
+﻿using MongoDB.Driver;
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
-using MongoDB.Driver;
 using XuongMay.Contract.Repositories.Interface;
 using XuongMay.Contract.Repositories.IUOW;
+using XuongMay.Repositories.UOW;
 
-namespace XuongMay.Repositories.UOW
+public class UnitOfWork : IUnitOfWork
 {
-    public class UnitOfWork : IUnitOfWork
+    private readonly IMongoDatabase _database;
+    private readonly IClientSessionHandle _session;
+    private readonly ConcurrentDictionary<Type, object> _repositories;
+
+    public UnitOfWork(IMongoClient mongoClient, IMongoDatabase database)
     {
-        private readonly IMongoDatabase _database;
-        private readonly IClientSessionHandle _session;
-        private readonly ConcurrentDictionary<Type, object> _repositories;
+        _database = database;
+        _session = mongoClient.StartSession();
+        _repositories = new ConcurrentDictionary<Type, object>();
+    }
 
-        public UnitOfWork(IMongoClient mongoClient, string databaseName)
-        {
-            _database = mongoClient.GetDatabase(databaseName);
-            _session = mongoClient.StartSession();
-            _repositories = new ConcurrentDictionary<Type, object>();
-        }
+    public IGenericRepository<T> GetRepository<T>() where T : class
+    {
+        return (IGenericRepository<T>)_repositories.GetOrAdd(typeof(T), (type) => new GenericRepository<T>(_database));
+    }
 
-        public IGenericRepository<T> GetRepository<T>() where T : class
-        {
-            return (IGenericRepository<T>)_repositories.GetOrAdd(typeof(T), (type) => new GenericRepository<T>(_database));
-        }
+    public void Save()
+    {
+        _session.CommitTransaction();
+    }
 
-        public void Save()
-        {
-            _session.CommitTransaction();
-        }
+    public async Task SaveAsync()
+    {
+        await _session.CommitTransactionAsync();
+    }
 
-        public async Task SaveAsync()
-        {
-            await _session.CommitTransactionAsync();
-        }
+    public void BeginTransaction()
+    {
+        _session.StartTransaction();
+    }
 
-        public void BeginTransaction()
-        {
-            _session.StartTransaction();
-        }
+    public void CommitTransaction()
+    {
+        _session.CommitTransaction();
+    }
 
-        public void CommitTransaction()
-        {
-            _session.CommitTransaction();
-        }
+    public void RollBackTransaction()
+    {
+        _session.AbortTransaction();
+    }
 
-        public void RollBackTransaction()
-        {
-            _session.AbortTransaction();
-        }
-
-        public void Dispose()
-        {
-            _session?.Dispose();
-        }
+    public void Dispose()
+    {
+        _session?.Dispose();
     }
 }

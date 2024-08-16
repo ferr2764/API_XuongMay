667448d1c78f412a436a752faa8e06b156fcbd2a
HoangVHSE184022 <145216389+HoangVHSE184022@users.noreply.github.com>
2024-08-17 03:30:25 +0700

    recover namespace

--- XuongMay.Services/Service/AccountService.cs
+++ XuongMay.Services/Service/AccountService.cs
@@ -2,48 +2,51 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 using XuongMay.Contract.Repositories.Entity;
 
-public class AccountService : IAccountService
+namespace XuongMay.Services.Service
 {
-    private readonly IMongoCollection<Account> _accounts;
+    public class AccountService : IAccountService
+    {
+        private readonly IMongoCollection<Account> _accounts;
 
-    public AccountService(IMongoDatabase database)
-    {
-        _accounts = database.GetCollection<Account>("Accounts");
-    }
+        public AccountService(IMongoDatabase database)
+        {
+            _accounts = database.GetCollection<Account>("Accounts");
+        }
 
-    //Get account By Id
-    public async Task<Account> GetAccountByIdAsync(string id)
-    {
-        ObjectId objectId = ObjectId.Parse(id);
-        return await _accounts.Find(a => a.Id == objectId).FirstOrDefaultAsync();
-    }
-    
-    //Get account By Role
-    public async Task<IEnumerable<Account>> GetAccountsByRoleAsync(string role)
-    {
-        return await _accounts.Find(a => a.Role == role).ToListAsync();
-    }
-    
-    //Get all accounts
-    public async Task<IEnumerable<Account>> GetAllAccountsAsync()
-    {
-        return await _accounts.Find(a => true).ToListAsync();
-    }
-   
-    //Update account
-    public async Task UpdateAccountByIdAsync(string id, Account updatedAccount)
-    {
-        ObjectId objectId = ObjectId.Parse(id);
+        //Get account By Id
+        public async Task<Account> GetAccountByIdAsync(string id)
+        {
+            ObjectId objectId = ObjectId.Parse(id);
+            return await _accounts.Find(a => a.Id == objectId).FirstOrDefaultAsync();
+        }
 
-        // Define the fields to update
-        var updateDefinition = Builders<Account>.Update
-            .Set(a => a.Name, updatedAccount.Name)
-            .Set(a => a.Username, updatedAccount.Username)
-            .Set(a => a.Password, updatedAccount.Password)
-            .Set(a => a.Role, updatedAccount.Role)
-            .Set(a => a.Salary, updatedAccount.Salary);
+        //Get account By Role
+        public async Task<IEnumerable<Account>> GetAccountsByRoleAsync(string role)
+        {
+            return await _accounts.Find(a => a.Role == role).ToListAsync();
+        }
 
-        // Execute the update
-        await _accounts.UpdateOneAsync(a => a.Id == objectId, updateDefinition);
+        //Get all accounts
+        public async Task<IEnumerable<Account>> GetAllAccountsAsync()
+        {
+            return await _accounts.Find(a => true).ToListAsync();
+        }
+
+        //Update account
+        public async Task UpdateAccountByIdAsync(string id, Account updatedAccount)
+        {
+            ObjectId objectId = ObjectId.Parse(id);
+
+            // Define the fields to update
+            var updateDefinition = Builders<Account>.Update
+                .Set(a => a.Name, updatedAccount.Name)
+                .Set(a => a.Username, updatedAccount.Username)
+                .Set(a => a.Password, updatedAccount.Password)
+                .Set(a => a.Role, updatedAccount.Role)
+                .Set(a => a.Salary, updatedAccount.Salary);
+
+            // Execute the update
+            await _accounts.UpdateOneAsync(a => a.Id == objectId, updateDefinition);
+        }
     }
 }

d93e85f562fd77db317cca0b0f08ae2ace43dcde
Hyudeque <quanghuy01062004@gmail.com>
2024-08-19 14:46:27 +0700

    Hide showing passwords, and add Pagination

---
+++ XuongMay.ModelViews/AccountModelView/ExposeAccountModelView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace XuongMay.ModelViews.AccountModelView
+{
+    public class ExposeAccountModelView
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+
+        public string? Role { get; set; }
+
+        public int? Salary { get; set; }
+
+        public string? Status { get; set; }
+    }
+}

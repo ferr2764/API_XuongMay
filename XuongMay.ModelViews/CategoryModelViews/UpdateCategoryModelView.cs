5f0cb2136bbbdba330db16a51e7e2de9e92721ef
NhatMinhz <trinhhhoangnhatminh123@gmail.com>
2024-08-20 15:22:54 +0700

    UpdateModelViewCategory

---
+++ XuongMay.ModelViews/CategoryModelViews/UpdateCategoryModelView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace XuongMay.ModelViews.CategoryModelViews
+{
+    public class UpdateCategoryModelView
+    {
+        public required string CategoryName { get; set; }
+        public required string CategoryDescription { get; set; }
+    }
+}
